9e55c606b5163273cd80a3da0d723ad50334b6ef
Dorosh-Max <57559932+Dorosh-Max@users.noreply.github.com>
2020-02-18 21:07:52 +0200

    disk
    disk

---
+++ твір/script.js
@@ -0,0 +1,12 @@
+let password = prompt (`password`);
+console.log(password)
+if(typeof age !== `string`){
+    if(age < SM_55541566){
+        console.log(` wrong password!`);
+    }else{
+            console.log(`Welcom`);
+        }
+    }else{
+        console.log(`enter the correct password!`);
+        
+    }
